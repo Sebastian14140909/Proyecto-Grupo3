a0b18633416905889858918fa209d2ed68c10592
Deivit Fernandez <75180525+Deivit99@users.noreply.github.com>
2025-04-18 11:50:37 -0600

    Se elimina el php cerrarsesion viejo
    Se elimina el php cerrarsesion viejo

--- Grupo 3/php/PHP_ CerrarSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();       // Limpia todas las variables de sesión
-session_destroy();     // Destruye la sesión actual
-
-// Redirige de vuelta al login
-header("Location: /ProyectoWeb/login.html");
-exit();
-?>
